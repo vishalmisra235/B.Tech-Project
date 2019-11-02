1c180aa20c961d288db6dc1c1c0e1a9a404fb133
vishalmisra235 <34361144+vishalmisra235@users.noreply.github.com>
2019-11-02 18:23:17 +0530

    Update cnn_classifier.py

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Dropout, MaxPool2D, Input, Softmax, Activation, Flatten
 from tensorflow.keras.models import Model
@@ -7,51 +6,50 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.layers import concatenate,AveragePooling2D
 import os
 import cv2
-import scipy.misc
+import scipy.misc as sc
 import matplotlib.pyplot as plt
 
 
 from tqdm import tqdm
 def get_data(folder):
 
-	X = []
-	y = []
-	z = []
-	for wbc_type in os.listdir(folder):
-		if not wbc_type.startswith('.'):
-            		if wbc_type in ['NEUTROPHIL']:
-                		label = 1
-                		label2 = 1
-            		elif wbc_type in ['EOSINOPHIL']:
-                		label = 2
-                		label2 = 1
-            		elif wbc_type in ['MONOCYTE']:
-                		label = 3  
-                		label2 = 0
-            		elif wbc_type in ['LYMPHOCYTE']:
-                		label = 4 
-                		label2 = 0
-            		else:
-                		label = 5
-                		label2 = 0
+    X = []
+    y = []
+    z = []
+    for wbc_type in os.listdir(folder):
+        if not wbc_type.startswith('.'):
+            if wbc_type in ['NEUTROPHIL']:
+                label = 1
+                label2 = 1
+            elif wbc_type in ['EOSINOPHIL']:
+                label = 2
+                label2 = 1
+            elif wbc_type in ['MONOCYTE']:
+                label = 3  
+                label2 = 0
+            elif wbc_type in ['LYMPHOCYTE']:
+                label = 4 
+                label2 = 0
+            else:
+                label = 5
+                label2 = 0
 
-		for image_filename in tqdm(os.listdir(folder + wbc_type)):
-			img_file = cv2.imread(folder + wbc_type + '/' + image_filename)
-			if img_file is not None:
-				img_file = scipy.misc.imresize(arr=img_file, size=(60, 80, 3))
-				img_arr = np.asarray(img_file)
-				X.append(img_arr)
-				y.append(label)
-				z.append(label2)
-	X = np.asarray(X)
-	y = np.asarray(y)
-	z = np.asarray(z)
-	return X,y,z
+        for image_filename in tqdm(os.listdir(folder + wbc_type)):
+            img_file = cv2.imread(folder + wbc_type + '/' + image_filename)
+            if img_file is not None:
+                img_file = sc.imresize(arr=img_file, size=(60, 80, 3))
+                img_arr = np.asarray(img_file)
+                X.append(img_arr)
+                y.append(label)
+                z.append(label2)
+    X = np.asarray(X)
+    y = np.asarray(y)
+    z = np.asarray(z)
+    return X,y,z
 
 
-X_train, y_train, z_train = get_data('/home/razorback/BTP/metamorphic_testing/blood-cells/dataset2-master/dataset2-master/images/TRAIN/')
-X_test, y_test, z_test = get_data('/home/razorback/BTP/metamorphic_testing/blood-cells/dataset2-master/dataset2-master/images/TEST/')
-
+X_train, y_train, z_train = get_data(r'C:\Users\Vishal\Documents\BTP\dataset2-master\dataset2-master\images\TRAIN\\')
+X_test, y_test, z_test = get_data(r'C:\Users\Vishal\Documents\BTP\dataset2-master\dataset2-master\images\TEST\\')
 # Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
 from tensorflow.keras.utils import to_categorical
 y_trainHot = to_categorical(y_train, num_classes = 5)
@@ -73,32 +71,34 @@ print("Train z Shape --> ",z_trainHot.shape)
 
 
 def keras_model():
-	inp = Input(shape=(60,80,3))
-	x = Conv2D(32, (11,11), padding="same",activation="relu")(inp)
-	x = Conv2D(32, (7,7), padding="valid",activation="relu")(inp)
-	x = MaxPool2D(pool_size=(2, 2))(x)
-	x = BatchNormalization()(x)
-	x = Dropout(0.5)(x)
-	x = Conv2D(32, (5, 5), padding="same",activation="relu")(x)
-	x = Conv2D(32, (5, 5), padding="valid",activation="relu")(x)
-	x = MaxPool2D(pool_size=(2, 2))(x)
-	x = BatchNormalization()(x)
-	x = Dropout(0.5)(x)
-	x = Conv2D(64, (3, 3), padding="same",activation="relu")(x)
-	x = Conv2D(64, (3, 3), padding="valid",activation="relu")(x)
-	x = MaxPool2D(pool_size=(2, 2))(x)
-	x = BatchNormalization()(x)
-	x = Dropout(0.5)(x)
-	x = Flatten()(x)
-	x = Dense(1024,activation="relu")(x)
-	x = Dropout(0.5)(x)
-	y = Dense(5,activation="softmax")(x)
-	#z = Dense(2,activation="softmax")(x)
-	model = Model(inp, y)
-	model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-	return model
+    inp = Input(shape=(60,80,3))
+    x = Conv2D(32, (11,11), padding="same",activation="relu")(inp)
+    x = Conv2D(32, (7,7), padding="valid",activation="relu")(inp)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(32, (5, 5), padding="same",activation="relu")(x)
+    x = Conv2D(32, (5, 5), padding="valid",activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(64, (3, 3), padding="same",activation="relu")(x)
+    x = Conv2D(64, (3, 3), padding="valid",activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    x = Flatten()(x)
+    print(x.shape)
+    print('Vishal')
+    x = Dense(1024,activation="relu")(x)
+    x = Dropout(0.5)(x)
+    y = Dense(5,activation="softmax")(x)
+    #z = Dense(2,activation="softmax")(x)
+    model = Model(inp, y)
+    model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+    return model
 
 
 model = keras_model()
@@ -106,13 +106,13 @@ model.summary()
 
 history = model.fit(X_train,
          y_trainHot,
-         epochs = 10,
+         epochs = 1,
          batch_size = 512,
          validation_data = (X_test,y_testHot),
          verbose = 1)
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
@@ -128,4 +128,3 @@ plt.ylabel('Loss')
 plt.legend()
 
 plt.show()
-
