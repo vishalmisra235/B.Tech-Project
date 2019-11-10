3a7e048048f4cfee6cafa995b8d9b2e330f84013
vishalmisra235 <34361144+vishalmisra235@users.noreply.github.com>
2019-11-10 22:34:32 +0530

    CNN classifier for traffic signs dataset

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -1,6 +1,4 @@
-
-## Code refernced from kaggle site
-
+#code referenced from kaggle kernel of gtsrb traffic sign dataset
 
 import numpy as np
 import pandas as pd
@@ -9,73 +7,87 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Dropout, MaxPool2D, Input, Softmax, Activation, Flatten, UpSampling2D
+from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Dropout, MaxPool2D, Input, Softmax, Activation, Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import concatenate,AveragePooling2D, MaxPooling2D
+from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.utils import to_categorical
 import os
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import scipy.misc
+import cv2
+from PIL import Image
+from tqdm import tqdm
 
-data_train = pd.read_csv("/home/razorback/BTP/metamorphic_testing/MNIST/fashionmnist/fashion-mnist_train.csv")
-data_test  = pd.read_csv("/home/razorback/BTP/metamorphic_testing/MNIST/fashionmnist/fashion-mnist_test.csv")
-img_rows, img_cols = 28, 28
-input_shape = (img_rows, img_cols, 1)
+data=[]
+labels=[]
 
-X = np.array(data_train.iloc[:, 1:])
-y = to_categorical(np.array(data_train.iloc[:, 0]))
+height = 30
+width = 30
+channels = 3
+classes = 43
+n_inputs = height * width * channels
 
-#Here we split validation data to optimiza classifier during training
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=13)
+for i in range(classes):
+	path = "/home/razorback/BTP/metamorphic_testing/traffic/gtsrb-german-traffic-sign/train/"
+	images = os.listdir(path+str(i))
 
-#Test data
-X_test = np.array(data_test.iloc[:, 1:])
-y_test = to_categorical(np.array(data_test.iloc[:, 0]))
+	for image in images:
+		try:
+			#print(path+image)
+			img = cv2.imread(path+str(i)+'/'+image)
+			#print(img)
+			if img is not None:
+				img_file = scipy.misc.imresize(arr=img, size=(height, width, channels))
+				img_arr = np.asarray(img_file)
+				data.append(img_arr)
+				labels.append(i)
+		except AttributeError:
+			print("vish")
+			
+
+Cells=np.asarray(data)
+print(Cells.shape)
+labels=np.array(labels)
+
+s=np.arange(Cells.shape[0])
+np.random.seed(43)
+np.random.shuffle(s)
+Cells=Cells[s]
+labels=labels[s]
 
 
+(X_train,X_val)=Cells[(int)(0.2*len(labels)):],Cells[:(int)(0.2*len(labels))]
+X_train = X_train.astype('float32')/255 
+X_val = X_val.astype('float32')/255
+(y_train,y_val)=labels[(int)(0.2*len(labels)):],labels[:(int)(0.2*len(labels))]
 
-X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-X_val = X_val.reshape(X_val.shape[0], img_rows, img_cols, 1)
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_val = X_val.astype('float32')
-X_train /= 255
-X_test /= 255
-X_val /= 255
-
+y_train = to_categorical(y_train, 43)
+y_val = to_categorical(y_val, 43)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 kernel_initializer='he_normal',
-                 input_shape=input_shape))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(Dropout(0.4))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.3))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+#Compilation of the model
+model.compile(
+    loss='categorical_crossentropy', 
+    optimizer='adam', 
+    metrics=['accuracy']
+)
 
+epochs = 20
+history = model.fit(X_train, y_train, batch_size=32, epochs=epochs,
+validation_data=(X_val, y_val))
 
-history = model.fit(X_train, y_train,
-          batch_size=256,
-          epochs=10,
-          verbose=1,
-          validation_data=(X_val, y_val))
-score = model.evaluate(X_test, y_test, verbose=0)
-
-
-
-
+		
